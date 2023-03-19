9fb8b77e5c222bf9e6fb82c2e28fac95869a78a3
Fabricio Paoloni <paolonifabricio@gmail.com>
2023-03-19 18:23:10 +0100

    added copyright at footer. Changed some styles in the carousel

--- scrypt.js
+++ scrypt.js
@@ -1,60 +1,67 @@
+
+/*Carousel logic starts*/
+
+/*Variable declarations*/
 const slidesContainer = document.getElementById("projects__slider");
 const slide = document.querySelector(".projects__slide");
 const slidesArray = Array.from(document.getElementsByClassName("projects__slide"));
 const dots = document.getElementsByClassName("projects__dot");
-let activeSlide = 0;
+
+let activeSlide = 0; //used to point the current slide displayed
 
 
-let sliderDebugger = true;
+let sliderDebugger = true; //used to prevent bugs while changing the slides
 
+//function used by the next and prev buttons to change the slide. parameter could be 1 (next slide) or -1 (previous slide)
 const plusSlides = (parameter) => {
-  if (sliderDebugger === true) {
-    sliderDebugger = false;
+  if (sliderDebugger === true) { //only executes the code if we says its ok
+    sliderDebugger = false; //prevent to executes again this code, wont change the slide
 
-    let slideWidth = slide.clientWidth;
-    activeSlide += parameter;
-    if(activeSlide < 0){
-      slidesContainer.scrollLeft = slideWidth * 3;
+    let slideWidth = slide.clientWidth; 
+    activeSlide += parameter; //gives us information of which slide we are going to see
+    if(activeSlide < 0){ //if we are in the first slide we want to move to the last one
+      slidesContainer.scrollLeft = slideWidth * slidesArray.length;
       activeSlide = slidesArray.length - 1;
-    } else if (activeSlide > slidesArray.length - 1){
-      slidesContainer.scrollLeft = slideWidth * (-3);
+    } else if (activeSlide > slidesArray.length - 1){ //if we are in the last one we move to the first
+      slidesContainer.scrollLeft = slideWidth * (-1) * slidesArray.length;
       activeSlide = 0;
     } else {
     slidesContainer.scrollLeft += slideWidth * (parameter);
   }
     
-
+  //this timeout will enable another slide change 
     const myTimeout = setTimeout(function(){
       sliderDebugger = true;
       clearTimeout(myTimeout);
     }, 500);
 
-    selectDotColor();
+    selectDotColor(); //change the color of the dot that point to the active slide
     
   }
 
 }
 
+//same as plusSlides but works for the dots that point to each slide. SlideNumber is the parameter that each dot passes to the function
 const currentSlide = (slideNumber) => {
-  if (sliderDebugger === true) {
+  if (sliderDebugger === true) { //prevent bugs while changing
     sliderDebugger = false;
     let slideWidth = slide.clientWidth;
-    let currentPosition = activeSlide * slideWidth;
-    let nextPosition = slideWidth * (slideNumber);
+    let currentPosition = activeSlide * slideWidth; //the position of the slider we are at the moment we activate this function
+    let nextPosition = slideWidth * (slideNumber); //the position we want to go
 
-    slidesContainer.scrollLeft += nextPosition - currentPosition;
-    activeSlide = slideNumber;
+    slidesContainer.scrollLeft += nextPosition - currentPosition; //we move the amout equivalent to the difference between the position we want to go and the position we are
+    activeSlide = slideNumber; //set the new active slide to keep track of the position
 
     const myTimeout = setTimeout(function(){
       sliderDebugger = true;
       clearTimeout(myTimeout);
     }, 700);
 
-    selectDotColor();
+    selectDotColor(); //change the color of the active dot
   }
-
 }
 
+//change the color of the dots
 const selectDotColor = () => {
   for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" projects__active", "");
@@ -62,113 +69,11 @@ const selectDotColor = () => {
     dots[activeSlide].className += " projects__active";
 }
 
-
-
-// const conteiner = document.getElementById("projects__container"),
-//       slides = Array.from(document.getElementsByClassName("projects__project-tile")),
-//       dots = document.getElementsByClassName("projects__dot"),
-//       imageCaptions = Array.from(document.getElementsByClassName("projects__image-caption"));
-
-
-// let slideIndex = 1;
-// let prevSlideIndex = 2;
-
-// let state = {
-//   isDragging: false,
-//   startPos: 0,
-//   currentTranslate: 0,
-//   prevTranslate: 0,
-//   animationID: 0,
-
-// }
-
-
-// showSlides(slideIndex);
-
-// // add event listener to our images (<a> elements in #projects)
-// slides.forEach((slide, index) => {
-//   slide.addEventListener('dragstart', (e) => {
-//     return e.preventDefault();
-//   });
-//   // 
-//   //touch events
-//   slide.addEventListener('touchstart', touchStart(index));
-//   slide.addEventListener('touchend', touchEnd);
-//   slide.addEventListener('touchmove', touchMove);
-// // 
-//   //Mouse events
-//   // slide.addEventListener('mousedown', touchStart(index));
-//   // slide.addEventListener('mouseup', touchEnd);
-//   // // slide.addEventListener('mouseleave', touchEnd);
-//   // slide.addEventListener('mousemove', touchMove);
-// // 
-// });
-
-// //disable context menu
-// window.oncontextmenu = function(event){
-//   event.preventDefault();
-//   event.stopPropagation();
-//   return false;
-// }
-
-// function touchStart(index){
-//   return function(event){
-//     state.startPos = event.type.includes("mouse") ? event.pageX : event.touches[0].clientX;
-//     state.isDragging = true;
-//   }
-// }
-
-// function touchEnd() {
-//   state.isDragging = false;
-//   const movedBy = state.currentTranslate - state.prevTranslate;
-//   if(movedBy < -100 && state.isDragging === false){
-//     plusSlides(1);
-//   } else if(movedBy > 100 && state.isDragging === false){
-//     plusSlides(-1);
-//   }
-
-// }
-
-// function touchMove(event) {
-//   if(state.isDragging){
-//     const currentPosition = event.type.includes("mouse") ? event.pageX : event.touches[0].clientX;
-//     state.currentTranslate = state.prevTranslate + currentPosition - state.startPos;
-//   }
-  
-// }
+/* CAROUSEL longic ends*/
 
 
 
-// //control previus and next buttons
-// const plusSlides = (n) => {
-//     prevSlideIndex = slideIndex;
-//     showSlides(slideIndex += n);
-// }
-
-// //little dots controls
-// function currentSlide(n){
-//     prevSlideIndex = slideIndex;
-//     showSlides(slideIndex = n);
-// }
-
-// function showSlides(n){
-//     if(n > slides.length){slideIndex = 1}
-//     if(n < 1){slideIndex = slides.length}
-//     //remove prevSlide display
-//     for (let i = 0; i < slides.length; i++) {
-//         slides[i].style.animation = "fade-out 0.25s";
-//         setTimeout(function(){
-//           slides[i].style.display = "none";
-//         }, 250);
-//       }
-//       for (let i = 0; i < dots.length; i++) {
-//         dots[i].className = dots[i].className.replace(" projects__active", "");
-//       }
-//     //add next slide display
-//     slides[slideIndex-1].style.animation = "fade-in 0.5s";
-//     setTimeout(function(){
-//       slides[slideIndex-1].style.display = "flex";
-//     }, 250);
-    
-//     dots[slideIndex-1].className += " projects__active";
-// }
+//get the current year for the copyright
+let d = new Date(); 
+let currentYear = d.getFullYear(); 
+document.getElementById("current-year").innerHTML = currentYear;
